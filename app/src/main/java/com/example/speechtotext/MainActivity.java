eab148b196c3254c7b47092f44e5eeee19b7c784
Shani sahu <shanisahu1012@gmail.com>
2019-09-14 13:05:44 +0530

    FirstPush

--- app/src/main/java/com/example/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -2,20 +2,30 @@ package com.example.speechtotext;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 TextView voiceInput,speakButton;
+Calendar  calendar; String timesysa; SimpleDateFormat simpleDateFormat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,9 +75,38 @@ TextView voiceInput,speakButton;
                     ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     voiceInput.setText(result.get(0));
                     //Save speech to text file
-
+                    writedatainfile(voiceInput.getText().toString());
                 }
+                break;
 
         }
     }
+
+    private void writedatainfile(String toString) {
+      calendar=Calendar.getInstance();
+      simpleDateFormat=new SimpleDateFormat("dd-MM-yyyy HH:ss");
+      timesysa="ExternalData"+timesysa+".txt";
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},0);//Checking permission
+        File folder= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File myFile = new File(folder,timesysa);//Filename 
+        writeData(myFile,toString);
+    }
+
+    private void writeData(File myFile, String result) {
+        FileOutputStream fileOutputStream=null;
+        try {
+            fileOutputStream = new FileOutputStream(myFile);
+            try {
+                fileOutputStream.write(result.getBytes());
+                Toast.makeText(this, "Done"+myFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
